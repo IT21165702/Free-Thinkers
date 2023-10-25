78ea5cc6deb5afab2b2b92da5517ee9fb6c43de0
IT21165702 <99709170+IT21165702@users.noreply.github.com>
2023-10-25 15:53:20 +0530

    avatar changes

--- GlamMate/cbot.py
+++ GlamMate/cbot.py
@@ -1,52 +1,104 @@
+# Import necessary libraries
 from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
 import random
 import pickle
 import numpy as np
-
 import nltk
-
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
 from nltk.tokenize import word_tokenize
 import nltk
+
+import mysql.connector  # Import the database connector library
+
+# Download NLTK data
 nltk.download('punkt')
 nltk.download("stopwords")
 nltk.download('wordnet')
 
-
+# Load intents data from a JSON file
 intents = json.loads(open('intents.json').read())
+# Load preprocessed data from pickle files
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
+# Function to establish a database connection
+def establish_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='12345678',
+            database='chatbotdb'
+        )
+        return connection
+    except Exception as e:
+        print(f"Database connection error: {str(e)}")
+        return None
+
+# Define a function to clean up a sentence for processing
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
+    # Tokenize the pattern - split words into an array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
+    # Lemmatize each word - create a short form for the word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
+
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
-def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+# Define a function to convert a sentence to a "bag of words" representation
+def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s:
-                # assign 1 if current word is in the vocabulary position
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
                 bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+    return np.array(bag)
 
-def predict_class(sentence, model):
+def get_order_by_id(order_id, db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM orders WHERE idorders = %s", (order_id,))
+    order_data = cursor.fetchone()
+    cursor.close()
+
+    if order_data:
+        return f"Here's your Order ->\n  Order ID: {order_data[0]}\n  Date: {order_data[1]}\n  Amount: {order_data[2]}\n  Shipping Address:{order_data[3]}\n  Order Status: {order_data[4]}\n  Order Details: {order_data[5]} "
+    else:
+        return "Order not found."
+
+# Define a function to get a response based on the predicted intent
+def getResponse(intents, intents_json, db_connection):
+    tag = intents[0]['intent']
+    list_of_intents = intents_json['intents']
+    for intent in list_of_intents:
+        if intent['tag'] == tag:
+            responses = intent['responses']
+            if tag == 'order_status':
+                order_id = intents[0]['order_id']
+                response = get_order_by_id(order_id, db_connection)
+                return response
+            else:
+                return random.choice(responses)
+
+    return "I'm sorry, I don't understand that."
+
+# Define a function to get a chatbot response(chatbot logic)
+def chatbot_response(text, db_connection):
+    if text.startswith("get order by ID"):
+        order_id = text.split()[-1]
+        order_data = get_order_by_id(order_id, db_connection)
+        return order_data
+    else:
+        return predict_class(text, db_connection)
+
+# Define a function to predict the intent of a sentence using the model
+def predict_class(sentence, db_connection):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bag_of_words(sentence)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
@@ -55,106 +107,29 @@ def predict_class(sentence, model):
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
+    response = getResponse(return_list, intents, db_connection)
+    return response
 
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
 
-def chatbot_response(text):
-    ints = predict_class(text, model)
-    res = getResponse(ints, intents)
-    return res
-
-"""GUI Interface
 
 """
+GUI Interface
+"""
+import tkinter as tk
+from PIL import Image, ImageTk
 
-import tkinter
-from tkinter import *
-
-BG_GRAY = "#ABB2B9"
 BG_COLOR = "#c5f0e3"
 TEXT_COLOR = "#000000"
-
-# BG_GRAY = "#ABB2B9"
-# BG_COLOR = "#1c172a"
-# TEXT_COLOR = "#ffffff"
-
-
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
+MAIN_BG_COLOR = "#6ac99d"
+CHAT_BUBBLE_COLOR = "#f4f4f4"
+ACCENT_COLOR = "#c79991"
 
-
-def send(event):
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#000000", font=("Verdana", 12 ))
-
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
-
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
-
-
-base = Tk()
-base.title("E-Commerce Chatbot")
-base.resizable(width=FALSE, height=FALSE)
-base.configure(width=800, height=800, bg=BG_COLOR)
-
-
-#Create Chat window
-ChatLog = Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT_BOLD)
-ChatLog.config(state=DISABLED)
-
-head_label = Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=FONT_BOLD, pady=10)
-head_label.place(relwidth=1)
-
-line = Label(base, width=450, bg=BG_GRAY)
-
-
-#Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
-ChatLog.focus()
-
-#Create Button to send message
-SendButton = Button(base, font=("Verdana", 12,'bold'), text="Send", width="12", height=15,
-                    bd=0, bg="#ed9061", activebackground="#3c9d9b",fg='#ffffff',
-                    command=lambda: send)
-
-#Create the box to enter message
-EntryBox = Text(base, bg="white",width="29", height="5", font="Arial", background="#dddddd")
-EntryBox.focus()
-EntryBox.bind("<Return>", send)
-#EntryBox.bind("<Return>", send)
-
-
-
-
-
-scrollbar.place(x=775,y=6, height=800)
-line.place(x=0,y=35, height=1, width=770)
-ChatLog.place(x=5,y=40, height=700, width=770)
-EntryBox.place(x=0, y=740, height=60, width=600)
-SendButton.place(x=600, y=740, height=60, width=175)
-
-import tkinter as tk
-
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#c5f0e3"
-TEXT_COLOR = "#000000"
-
+# Define the send function
 def send():
+    #Establish a database connection
+    db_connection = establish_db_connection()
     msg = entry_box.get()
     entry_box.delete(0, tk.END)
     if msg.strip() != '':
@@ -162,106 +137,74 @@ def send():
         chat_log.insert(tk.END, "You: " + msg + '\n\n')
         chat_log.config(foreground="#000000", font=("Verdana", 12))
 
-        res = chatbot_response(msg)  # Replace with your chatbot logic
+        res = chatbot_response(msg, db_connection)
         chat_log.insert(tk.END, "Bot: " + res + '\n\n')
-
         chat_log.config(state=tk.DISABLED)
         chat_log.yview(tk.END)
 
+        #Close the database connection when done
+        db_connection.close()    
+
+
+# # Establish a database connection
+# db_connection = establish_db_connection()
+
+# if db_connection:
+#     print("GO! CHATBOT IS RUNNING!")
+
+#     while True:
+#         message = input("You: ")
+#         response = chatbot_response(message, db_connection)
+#         print("Chatbot:", response)
+
+#     # Close the database connection when done
+#     db_connection.close()
+# else:
+#     print("Unable to connect to the database. Please check your database settings.")
+
+
 base = tk.Tk()
-base.title("E-Commerce Chatbot")
-base.geometry("800x800")
+base.title("GlamMate Chatbot")
+base.geometry("800x600")
 
-chat_log = tk.Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=("Helvetica 13 bold"))
+# Top Frame for Avatar and Head Label
+top_frame = tk.Frame(base, bg=BG_COLOR)
+top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+# Load and resize the avatar image
+avatar_image = Image.open("avatar.jpg")
+avatar_image.thumbnail((50, 50))
+avatar_photo = ImageTk.PhotoImage(avatar_image)
+
+bot_avatar_label = tk.Label(top_frame, image=avatar_photo, bg=BG_COLOR)
+bot_avatar_label.image = avatar_photo
+bot_avatar_label.pack(side=tk.LEFT, padx=(10, 5), pady=10)
+
+head_label = tk.Label(top_frame, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to GlamMate Chatbot", font=("Helvetica 16 bold"), pady=10)
+head_label.pack(side=tk.LEFT)
+
+# Middle Frame for Chat Screen with Scrollbar
+middle_frame = tk.Frame(base, bg=BG_COLOR)
+middle_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=0)  # Adjust pady to 0
+
+chat_log = tk.Text(middle_frame, bd=0, bg=MAIN_BG_COLOR, fg=TEXT_COLOR, font=("Lato", 12))
 chat_log.config(state=tk.DISABLED)
+chat_log.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=0)  # Adjust pady to 0
 
-head_label = tk.Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=("Helvetica 13 bold"), pady=10)
-head_label.pack(fill=tk.X)
-
-entry_box = tk.Entry(base, bg="white", font=("Arial", 12))
-entry_box.bind("<Return>", lambda event=None: send())
-entry_box.pack(fill=tk.BOTH, padx=5, pady=5)
-
-send_button = tk.Button(base, text="Send", command=send, font=("Verdana", 12, 'bold'), bg="#ed9061", activebackground="#3c9d9b", fg='#ffffff')
-send_button.pack(fill=tk.BOTH)
-
-scrollbar = tk.Scrollbar(base, command=chat_log.yview, cursor="heart")
-chat_log['yscrollcommand'] = scrollbar.set
+# Add a scrollbar for the chat log
+scrollbar = tk.Scrollbar(middle_frame, command=chat_log.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+chat_log.config(yscrollcommand=scrollbar.set)
 
-chat_log.pack(fill=tk.BOTH, expand=True)
+# Bottom Frame for Input Box and Send Button
+bottom_frame = tk.Frame(base, bg=BG_COLOR)
+bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-base.mainloop()
+entry_box = tk.Entry(bottom_frame, bg="white", font=("Lato", 18), width=50)
+entry_box.bind("<Return>", lambda event=None: send())
+entry_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-
-
-import tkinter
-from tkinter import *
-
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#c5f0e3"
-TEXT_COLOR = "#000000"
-
-# BG_GRAY = "#ABB2B9"
-# BG_COLOR = "#1c172a"
-# TEXT_COLOR = "#ffffff"
-
-
-FONT = "Helvetica 14"
-FONT_BOLD = "Helvetica 13 bold"
-
-
-def send(event):
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#000000", font=("Verdana", 12 ))
-
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
-
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
-
-
-base = Tk()
-base.title("E-Commerce Chatbot")
-base.resizable(width=FALSE, height=FALSE)
-base.configure(width=800, height=800, bg=BG_COLOR)
-
-
-#Create Chat window
-ChatLog = Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT_BOLD)
-ChatLog.config(state=DISABLED)
-
-head_label = Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=FONT_BOLD, pady=10)
-head_label.place(relwidth=1)
-
-line = Label(base, width=450, bg=BG_GRAY)
-
-
-#Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
-ChatLog.focus()
-
-#Create Button to send message
-SendButton = Button(base, font=("Verdana", 12,'bold'), text="Send", width="12", height=15,
-                    bd=0, bg="#ed9061", activebackground="#3c9d9b",fg='#ffffff',
-                    command=lambda: send)
-
-
-EntryBox = Text(base, bg="white",width="29", height="5", font="Arial", background="#dddddd")
-EntryBox.focus()
-EntryBox.bind("<Return>", send)
-
-
-scrollbar.place(x=775,y=6, height=800)
-line.place(x=0,y=35, height=1, width=770)
-ChatLog.place(x=5,y=40, height=700, width=770)
-EntryBox.place(x=0, y=740, height=60, width=600)
-SendButton.place(x=600, y=740, height=60, width=175)
+send_button = tk.Button(bottom_frame, text="Send", command=send, font=("Lato", 16, 'bold'), bg=ACCENT_COLOR, fg='white')
+send_button.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 base.mainloop()
